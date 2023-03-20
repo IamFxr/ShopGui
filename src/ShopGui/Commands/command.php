be4941aef9878164713fd1a59ec2cc7ee9642248
ShxdowBadDev <128105244+ShxdowBadDev@users.noreply.github.com>
2023-03-20 09:10:23 -0700

    Create command.php

---
+++ src/ShopGui/Commands/command.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShopGui\Commands;
+
+use ShopGui\ShopGui;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class command extends Command
+{
+public function __construct()
+    {
+        parent::__construct('shopgui', 'open the shopgui!');
+}
+ /**
+     * @param CommandSender $sender
+     * @param string $commandLabel
+     * @param array $args
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void
+    {
+
+if (!isset($args[0])) {
+
+ShopGui::OpenShopFormat($sender);
+return;
+}
+            
+            switch ($args[0]) {
+                case "blocks":
+                   
+ShopGui::OpenBlockShop($sender);
+
+break;
+
+case "equipment":
+
+ShopGui::OpenEquipmentShop($sender);
+
+break;
+
+case "food":
+
+ShopGui::OpenFoodShop($sender);
+break;
+
+}
+} 
